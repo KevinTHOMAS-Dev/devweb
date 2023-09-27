e196b3e51e1cce9b1e2d908f245bda427cfcadc6
KevinTHOMAS-Dev <128711174+KevinTHOMAS-Dev@users.noreply.github.com>
2023-09-27 22:40:41 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@
 
 const page = () => {
     return (
-        <section className="home">
-            <h1>Home</h1>            
+        <section className="home">   
+            <div className="main_home">
+                <h1>Home</h1>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis aperiam beatae incidunt dolore cum ab consectetur rerum, ipsa consequuntur! Ex veniam in excepturi, similique consectetur fugiat, eligendi quis illum magnam impedit quia at iure aperiam doloremque aliquam neque sapiente incidunt eveniet. Voluptatum quo ex enim, fuga molestias vel porro quos quam numquam dicta aut accusantium amet eum assumenda reiciendis atque, ipsa iure rem est vero nemo quaerat natus impedit incidunt. Fugit asperiores tempore, blanditiis provident beatae odit ab non est sapiente et tenetur repellat eveniet fuga, quis esse ducimus exercitationem quasi obcaecati accusamus? Odit, rem assumenda architecto sint eos ipsum._g</p>
+            </div>
         </section>
     );
 };
